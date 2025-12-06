28be58cbbb14e267b02c4308ac71ffae296b171c
Adarshsrg1999 <112505027+Adarshsrg1999@users.noreply.github.com>
2025-12-06 15:31:22 +0530

    fix: Resolve unused var in Admin.jsx

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { resetDatabase } from '../api';
 import ConfirmationModal from '../components/ConfirmationModal';
 
